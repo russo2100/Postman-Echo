fc854edd0b16c1e92cb97ad131765360cf5a2a24
russo2100 <russolatypov@gmail.com>
2022-08-06 15:16:12 +0400

    commit messege

--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV5 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(400)
+          .statusCode(200)
           .contentType(ContentType.JSON)
           .body("every{ it.balance >= 0 }", is(true))
       ;
