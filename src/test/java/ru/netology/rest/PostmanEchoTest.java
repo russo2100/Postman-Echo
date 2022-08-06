2826ef331bef5307ce801e032b0100005bd18b07
russo2100 <russolatypov@gmail.com>
2022-08-06 16:33:00 +0400

    first commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as response body"));
+    }
+    
+
+
+}
